8cbc17278953ca9016745bb80dd73c57fc0c528e
Mamdouh Sawy <81479655+Elsawy9879@users.noreply.github.com>
2025-07-22 01:33:18 +0300

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -10,6 +10,17 @@ export default function Portfolio() {
 
   return (
     <>
+      <div className="pt-4">
+      <div className="text-center header pt-4" style={{ color: "#2c3e50" }}>
+        <h2 className="text-uppercase mb-3 fs-1 fw-bolder">
+          portfolio component
+        </h2>
+        <div className="d-flex align-items-center justify-content-center mb-3">
+          <div className="line-p me-3"></div>
+          <i className="fa-solid fa-star" style={{ color: "#2c3e50" }}></i>
+          <div className="line-p ms-3"></div>
+        </div>
+      </div>
       <div className="portfolio-grid">
         {images.map((img, index) => (
           <div className="portfolio-item" key={index}>
