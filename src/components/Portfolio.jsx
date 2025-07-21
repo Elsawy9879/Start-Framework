c550ac226f98c13dd1599eb31ee06d3dd49e838a
Mamdouh Sawy <81479655+Elsawy9879@users.noreply.github.com>
2025-07-22 01:13:30 +0300

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
 import "./Portfolio.css";
-
-// استبدل الصور بمساراتك
 import port1 from "../assets/port1.png";
 import port2 from "../assets/port2.png";
 import port3 from "../assets/port3.png";
 
 export default function Portfolio() {
   const images = [port1, port2, port3, port1, port2, port3];
-
-  const [popupImage, setPopupImage] = useState(null); // لتخزين الصورة المختارة
+  const [popupImage, setPopupImage] = useState(null);
 
   return (
     <>
@@ -18,19 +15,19 @@ export default function Portfolio() {
           <div className="portfolio-item" key={index}>
             <div className="image-box" onClick={() => setPopupImage(img)}>
               <img src={img} alt="portfolio" className="portfolio-img" />
+              <div className="layer__">+</div>
             </div>
           </div>
         ))}
       </div>
 
-      {/* عند الضغط تظهر الصورة وسط الشاشة */}
       {popupImage && (
         <div className="popup-container" onClick={() => setPopupImage(null)}>
           <img
             src={popupImage}
-            alt="popup"
+            alt="Zoomed"
             className="popup-image"
-            onClick={(e) => e.stopPropagation()} // عشان الصورة نفسها ما تقفلش لما تضغط عليها
+            onClick={(e) => e.stopPropagation()}
           />
         </div>
       )}
